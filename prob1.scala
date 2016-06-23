607bffdbfab4d92402796a9c7b21e6f36f0c5056
mingik <minkitmih@gmail.com>
2016-06-22 21:18:49 -0700

    Update prob1.scala

--- prob1.scala
+++ prob1.scala
@@ -1,1 +1,8 @@
-println( (1 to 999).filter( (i:Int) => (i % 3 == 0) || (i % 5 == 0)).reduce(_+_) );
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+scala> (1 to 1000).filter(n => (n % 3 == 0) || (n % 5 == 0)).sum
+res1: Int = 234168
