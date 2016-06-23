574f6baa23b2c72785879d1abe00f0fda4a1dfae
mingik <mingiyan@gmail.com>
2016-06-22 22:12:11 -0700

    first 3 problems

---
+++ problem3.scala
@@ -0,0 +1,13 @@
+/*
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+ */
+
+def isPrime(n: Long) = !2L.to(Math.sqrt(n).toLong).exists(n % _ == 0)
+
+Math.sqrt(600851475143d).toLong.to(2).by(-1).collectFirst{ case m if (BigInt(600851475143l) % m == 0 && isPrime(m)) => m}
+
+// res1: Option[Long] = Some(6857)
